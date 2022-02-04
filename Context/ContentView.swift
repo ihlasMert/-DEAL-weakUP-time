875ccd52e4348a23341c1fe4474bd7fae4fd75c5
İhlas Mert <88083556+ihlasMert@users.noreply.github.com>
2022-02-04 17:46:20 +0300

    changed Date Components

--- Context/ContentView.swift
+++ Context/ContentView.swift
@@ -8,17 +8,29 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View{
-@State private var wakeUp = Date.now
+@State private var wakeUp = defaultWakeTime
 @State private var sleepAmount = 8.0
-@State private var coffeeAmount = 1
+@State private var coffeeAmount = 1 //eklenecek olan miktar
+    
 @State private var alertTitle = ""
 @State private var alertMessage = ""
-     @State private var showinAlert = false
+@State private var showinAlert = false
+    
+
+        
+   static var defaultWakeTime: Date{
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+            
+        }
+  
 
 
     var body: some View{
         NavigationView{
-            VStack{
+        Form{
                 Text("When do you want to wake up?")
                     .font(.headline)
                 DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
@@ -26,12 +38,18 @@ struct ContentView: View{
                 
                 Text("Desire amount of sleep")
                     .font(.headline)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(9)
                 Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 1...12, step: 0.25)
                 
                 
                 Text("Daily coffee intake")
                     .font(.headline)
-                Stepper(coffeeAmount == 1 ? "1 cup": "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(9)
+                Stepper(coffeeAmount == 1 ?  "1 cup": "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
             }
             .navigationTitle("Better Rest")
             .toolbar{
